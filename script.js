1b4c2b173207f0820665068155fcf6e61663e551
isman arale <ismailarales@gmail.com>
2022-12-29 18:37:35 -0600

    cleaned up code and added comments

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const dice = document.getElementById("dice");
 dice.addEventListener("click", function () {
   const adviceData = async function () {
     try {
+      // get advice data from api
       let num = Math.floor(Math.random() * 224);
       const res = await fetch(`https://api.adviceslip.com/advice/${num}`);
       const data = await res.json();
-      console.log(data.slip);
+
+      // change UI upon clicking the dice
       adviceText.style.color = `#cee3e9`;
       adviceNumber.innerHTML = `Advice #${data.slip.id}`;
       adviceText.innerHTML = `${data.slip.advice}`;
     } catch (err) {
+      // display error in red clr
       adviceText.style.color = "crimson";
       adviceText.innerHTML = `An error occured, please wait a second and generate another advice or reload the page 😓😓${err}`;
     }
